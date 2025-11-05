67ae536c2fdbdb01668c089c4dad774e112e3d2e
Rexezuge <79224316+Rexezuge@users.noreply.github.com>
2025-11-05 02:46:04 +0000

    FEAT: able to login now

--- src/index.ts
+++ src/index.ts
@@ -18,6 +18,19 @@ export default {
         await page.keyboard.type('password', { delay: 50 });
         await page.keyboard.press('Enter');
         await sleep(1000);
+        const totpUrl = 'https://totp-generator.2ba35e4d622c4747d091cb066978b585.workers.dev/generate-totp?key=sometotpkey123&digits=6&period=30&algorithm=SHA-1';
+        const response = await fetch(totpUrl);
+           if (!response.ok) {
+            throw new Error("failed to get totp")
+      }
+       const data = await response.json();
+      const otp = data.otp;
+        await page.keyboard.type(otp, { delay: 50 });
+        await page.keyboard.press('Enter');
+        await sleep(3000);
+// await page.waitForSelector('[data-testid="secondaryButton"]', { visible: true });
+await page.click('[data-testid="secondaryButton"]');
+await sleep(5000)
         img = (await page.screenshot()) as Buffer;
         await browser.close();
       return new Response(img, {
