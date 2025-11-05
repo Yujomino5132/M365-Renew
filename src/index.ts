ee03e4120127ff9a69e6a124551e7327e005e49a
Rexezuge <79224316+Rexezuge@users.noreply.github.com>
2025-11-05 02:16:19 +0000

    FEAT: types username and password

--- src/index.ts
+++ src/index.ts
@@ -2,34 +2,32 @@ import puppeteer from "@cloudflare/puppeteer";
 
 interface Env {
   MYBROWSER: Fetcher;
-  BROWSER_KV_DEMO: KVNamespace;
 }
 
 export default {
   async fetch(request, env): Promise<Response> {
-    const { searchParams } = new URL(request.url);
-    let url = searchParams.get("url");
+    let url = "https://www.microsoft.com/cascadeauth/store/account/signin"
     let img: Buffer;
-    if (url) {
       url = new URL(url).toString(); // normalize
-      img = await env.BROWSER_KV_DEMO.get(url, { type: "arrayBuffer" });
-      if (img === null) {
         const browser = await puppeteer.launch(env.MYBROWSER);
         const page = await browser.newPage();
         await page.goto(url);
+        await page.keyboard.type('username@outlook.com', { delay: 50 });
+        await page.keyboard.press('Enter');
+        await sleep(1000);
+        await page.keyboard.type('password', { delay: 50 });
+        await page.keyboard.press('Enter');
+        await sleep(1000);
         img = (await page.screenshot()) as Buffer;
-        await env.BROWSER_KV_DEMO.put(url, img, {
-          expirationTtl: 60 * 60 * 24,
-        });
         await browser.close();
-      }
       return new Response(img, {
         headers: {
           "content-type": "image/jpeg",
         },
       });
-    } else {
-      return new Response("Please add an ?url=https://example.com/ parameter");
-    }
   },
 } satisfies ExportedHandler<Env>;
+
+function sleep(ms: number): Promise<void> {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
